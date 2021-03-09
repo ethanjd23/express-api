6abd829e114bc18d7fd8c5f6e8b9b971f293519e
ethanjd23 <ethanjd23@gmail.com>
2021-03-09 14:45:58 -0600

    front end initial  get working

--- client/app.js
+++ client/app.js
@@ -1,3 +1,30 @@
-$.get("/chirps.json", function(data) {
-    alert("success!");
-})
+$(function () {
+  // I read that this is the current syntax to write document.ready, I promise.
+  console.log("script working");
+  $.get("/api/chirps", function (json) {
+    let ids = Object.keys(json);
+
+    let chirpsArray = ids.map((id) => {
+      return {
+        id: id,
+        user: json[id].user,
+        message: json[id].message,
+      };
+    }); // Creating an array of objects from the JSON
+    chirpsArray.pop(); // deleting nextid off each object
+
+    chirpsArray
+      .slice(0)
+      .reverse() // This flips the order of the array, ik it's gross. 
+      .forEach((chirp) => {
+        $("#chirps").append(
+          `<div class="card m-3 col-4">
+        <p class="card-header">${chirp.user}</p>
+        <div class="card-body">
+            <h5 class="card-text">${chirp.message}</h5>
+        </div>
+    </div>`
+        );
+      });
+  });
+});
