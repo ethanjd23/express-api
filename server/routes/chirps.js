bcaa38a6c913d5c9348cf09e00c5561e59feb82f
ethanjd23 <ethanjd23@gmail.com>
2021-03-10 10:27:11 -0600

    found out my delete function was was mistyped in chirps.js, but now it's saying it's now a function for some reason.

--- server/routes/chirps.js
+++ server/routes/chirps.js
@@ -22,7 +22,7 @@ router.put('/:id?', (req, res) => {
 })
 
 router.delete('/:id?', (req, res) => {
-    chirpsStore.DeleteChirp(req.params.id);
+    chirpsStore.deleteChirp(req.params.id);
 })
 
 module.exports = router;
